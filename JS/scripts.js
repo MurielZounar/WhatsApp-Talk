1b2614599fef505680dc67521f744bf96bcd2b4d
Muriel Zounar <muriel.zounar@gmail.com>
2022-06-29 13:10:17 -0300

    conclusão

--- JS/scripts.js
+++ JS/scripts.js
@@ -9,7 +9,7 @@ function setOption(country) {
       number += element
     }
   }
-  option.innerHTML = '+' + number + ' - ' + country.iso3
+  option.innerHTML = '+' + number + ' ' + country.iso3
   list.appendChild(option)
 }
 
@@ -23,6 +23,42 @@ function getCountries(url) {
         setOption(element)
       })
     })
+}
+
+function openNewTab(txt) {
+  const url = 'https://api.whatsapp.com/send?phone=' + txt
+  const win = window.open(url, '_blank')
+  win.focus
+}
+
+function getText() {
+  let country = document.getElementById('count-name').value
+  let phoneNum = document.getElementById('inp-num').value
+  let msg = document.getElementById('inp-msg').value
+  let countryNum = ''
+  let message = ''
+
+  for (let i = 0; i < country.length; i++) {
+    const num = country[i]
+
+    if (num >= 0 && num <= 9) {
+      countryNum += num
+    }
+  }
+
+  for (let i = 0; i < msg.length; i++) {
+    const newMsg = msg[i]
+
+    if (newMsg != ' ') {
+      message += newMsg
+    } else {
+      message += '%20'
+    }
+  }
+
+  let text = countryNum + phoneNum + '&text=' + message
+
+  openNewTab(text)
 }
 
 getCountries('assets/JSON/countries-JSON/countriesJson_ptBR.json')
